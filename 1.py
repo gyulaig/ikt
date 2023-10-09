e4d93950a3b3f28fee9acf80b73e64548829264d
Gyulai Gergő Zsolt <144451652+gyulaig@users.noreply.github.com>
2023-10-09 10:02:16 +0200

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,2 @@
+kilometer = float(input('ajd meg egy tavolsagot kilometerben: '))
+print(f'megadott kilometer nautical mileben: {kilometer / 1852}')
---
+++ 2.py
@@ -0,0 +1,3 @@
+szam1 = int(input('adj meg egy szamot: '))
+szam2 = int(input('adj meg meg egy szamot: '))
+print(f'{szam1} es {szam2} osszege: {szam1 + szam2}')
